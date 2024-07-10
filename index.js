4310b3c0638314114fed8692d1a3298ed0a310a2
AGreaves99 <ajohngreaves@outlook.com>
2024-07-10 10:17:34 +0100

    changes io server to be attached to an http server

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-import { Server } from 'socket.io';
-import { postNewMessage } from './api.js';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { postNewMessage } from "./api.js";
 
-const io = new Server({
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
   cors: {
-    origin: true,
-  }
+    origin: "*",
+  },
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`connected: ${socket.id} \n`);
 
-  socket.on('chat-message', (message, room) => {
-    socket.to(room).emit('receive-message', message)
-    console.log('Updating the database...')
+  socket.on("chat-message", (message, room) => {
+    socket.to(room).emit("receive-message", message);
+    console.log("Updating the database...");
     postNewMessage(message, room).then((message) => {
-      console.log("DB updated successfully")
-    })
-  })
+      console.log("DB updated successfully");
+    });
+  });
 
-  socket.on('join-room', room => {
-    socket.join(room)
-  })
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
 });
 
-console.log("Listening on port 3000")
+console.log("Listening on port 3000");
 io.listen(3000);
